8553b0177201148dd1485bdee90d6b35f24ae3fc
Safaroff90 <110519574+Safaroff90@users.noreply.github.com>
2022-12-13 01:07:51 +0400

    Add Register Form

---
+++ _Pustok/Controllers/AccountController.cs
@@ -0,0 +1,69 @@
+﻿using _Pustok.DAL;
+using _Pustok.Models;
+using _Pustok.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace _Pustok.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly PustokContext _pustokContext;
+        private readonly UserManager<AppUser> _userManager;
+
+        public AccountController(PustokContext pustokContext,UserManager<AppUser> userManager)
+        {
+            this._pustokContext = pustokContext;
+            this._userManager = userManager;
+        }
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult Register()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> Register(MemberRegisterVM memberVM)
+        {
+            if(!ModelState.IsValid)
+                return View();
+
+            //AppUser user = _pustokContext.AppUsers.FirstOrDefault(x => x.NormalizedUserName == memberVM.Username.ToUpper());
+           
+
+            if(await _userManager.FindByNameAsync(memberVM.Username) != null)
+            {
+                ModelState.AddModelError("Username", "User alrady exist");
+                return View();
+            }
+            else if(await _userManager.FindByEmailAsync(memberVM.Email) != null)
+            {
+                ModelState.AddModelError("Email", "Email alrady exist");
+                return View();
+            }
+
+            AppUser user = new AppUser
+            {
+                UserName = memberVM.Username,
+                Email = memberVM.Email,
+                Fullname = memberVM.Fullname,
+            };
+           var result = await _userManager.CreateAsync(user, memberVM.Password);
+
+            if (!result.Succeeded)
+            {
+                foreach(var err in result.Errors)
+                {
+                    ModelState.AddModelError("", err.Description);
+                }
+
+                return View();
+            }
+
+            return RedirectToAction("login");
+        }
+    }
+}
